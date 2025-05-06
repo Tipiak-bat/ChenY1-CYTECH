9bd8b8453128de2c9a9496b69a76ff9b45cf1b44
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-06 11:16:15 +0200

    Create arraymanaging.c

---
+++ arraymanaging.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int printTab(int size, Animal * animals){
+    if(size < 0 || animals == NULL) {
+        printf("[ERROR] Paramètres invalides : animals est NULL ou size < 0\n");
+    }
+    for(int i = 0; i < size; i++) {
+        if(animals[i].keyid < 0) {
+            continue;
+        }
+        printf("%d %s %d %d %d %s\n\n",
+                      animals[i].keyid,
+                      animals[i].nom,
+                      animals[i].espece,
+                      animals[i].b_year,
+                      animals[i].weight,
+                      animals[i].phrase);
+    }
+    return 0;
+}
+
+// Fonction pour ajouter un animal VIA une demande utilisateur (scanf, printf, etc)...
+int emptyCages(Animal * animals, int size) {
+    int nmb = 0;
+    for(int i = 0; i < size; i++){
+        if(animals.keyid[i] < 0 ) {
+            nmb++;
+        }
+    }
+    return nmb;
+}
+int addAnimal(Animal *animals, int size) {
+    if (animals == NULL || size <= 0) {
+        printf("[ERROR] Paramètres invalides : animals est NULL ou size <= 0\n");
+        return 1;
+    }
+
+
+    printf("Entrez les informations de l'animal (keyid, nom, espece, b_year, weight, phrase) :\n");
+    scanf("%d %s %d %d %d %
+
+    return 0;
+}
