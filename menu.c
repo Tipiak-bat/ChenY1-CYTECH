fe74a65774195e226e1474ce8b4b5a4664528847
Aryem03 <aryemzitoune@gmail.com>
2025-05-09 10:05:36 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,6 +1,15 @@
 #include "menu.h"
 
+void Rechercher_animal(){
+printf ("
+}
+void Ajouter_animal(){
 
+}
+void Supprimer
+
+
+do {                           // faire boucle do....while pour que le menu s'affiche plusieurs fois
 void affiche_menu () {
   printf （"\n====Bienvenue dans Le menu de ChenY1 le Refuge====\n"）；
   printf("1: Vous Recherchez un animal\n");
@@ -15,34 +24,32 @@ void affiche_menu () {
 
 int choix; 
 
-            // faire boucle do....while pour que le menu s'affiche plusieurs fois
-do {
 affiche_menu();
 printf("Quel est votre choix\n");     // on regarde ce qu'a choisie l'utilisateur
 scanf ("%d", &choix);
                                      // utiliser switch case car il y a plusieurs valeur
 switch (choix) {
-case 1 : 
-        Rechercher_animal();
-        break;
-case 2 : 
-        Ajouter_animal();
-        break ;
-case 3 :
-        Supprimer_animal();
-        break;
-case 4 : 
-        Inventaire_par_espece();
-        break;
-case 5 :
-        Nettoyage_hebdomadaire();
-        break;
-case 6 : 
-        Quitter_le_Menu();
-        break;
+        case 1 : 
+               Rechercher_animal();
+               break;
+        case 2 : 
+               Ajouter_animal();
+               break ;
+        case 3 :
+               Supprimer_animal();
+               break;
+        case 4 : 
+               Inventaire_par_espece();
+               break;
+        case 5 :
+               Nettoyage_hebdomadaire();
+               break; 
+        case 6 : 
+               Quitter_le_Menu();
+               break;
 default:
         printf("Erreur: veuiller saisir un numéro entre 1 et 6");
-while(choix != 6);                       // ne pas quitter tant que l'utilisateur na pas choisie le numéro 6
+while(choix > 0 || choix > 6);                       // ne pas quitter tant que l'utilisateur na pas choisie u nemero entre un et 6
 
 return 0; 
 }
