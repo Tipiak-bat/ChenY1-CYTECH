5ade94f5834fbee311cd0c7768748ba9efed2094
Aryem03 <aryemzitoune@gmail.com>
2025-05-10 13:33:02 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,3 +1,28 @@
 #include<stdio.h>
 
+
+
+//c'est pour aryem quand j'ai fait la fonction ajouter animal je savais pas ou mettre desoler 
+ typedef struct {
+    int identifiant;
+    char nom[50];
+    int espece;
+    int annee_naissance;
+    float poids;
+    char categorie_age[10];
+} Animal;
+
+// pour la fonction nettoyage hebdomadaire 
+typedef struct {
+    int id;
+    char nom[50];
+    int espece;
+} Animal;
+
+// pour la fonction supprimer animal 
+typedef struct {
+    int id;
+    char nom[50];
+} Animal; 
+
 void affiche_Menu();
