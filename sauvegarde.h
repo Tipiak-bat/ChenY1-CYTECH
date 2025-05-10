b62b04cd8f15ecc1075b73ff48b66944276445cf
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-10 18:22:32 +0200

    Create sauvegarde.h

---
+++ sauvegarde.h
@@ -0,0 +1,25 @@
+#ifndef SAUVEGARDE_H
+#define SAUVEGARDE_H
+
+#include <stdio.h>
+
+typedef struct
+{
+    int keyid;
+    char nom[25];
+    int espece;
+    int b_year;
+    float weight;
+    char phrase[255];
+} Animal;
+
+
+typedef enum
+{
+    HAMSTERE = 1,
+    AUTRUCHE = 2,
+    CHAT = 3,
+    CHIEN = 4
+} Espece;
+
+#endif //SAUVEGARDE_H
