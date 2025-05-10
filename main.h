bfbc47308b83d723c7eea8051c2b8e5e5c7d771f
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-10 18:21:31 +0200

    Update main.h

--- main.h
+++ main.h
@@ -8,26 +8,30 @@
    #include <time.h>
    #define SIZE 50
 
-typedef struct
- {
+typedef struct Animal {
     int keyid;
     char nom[25];
     int espece;
     int b_year;
-    int weight;
+    float weight;
     char phrase[255];
- } Animal;
+} Animal;
 
 
-typedef enum
-{
-    INDEFINI = 0,
+
+typedef enum Espece{
     HAMSTERE = 1,
     AUTRUCHE = 2,
     CHAT = 3,
     CHIEN = 4
 } Espece;
 
- Animal animals[SIZE];
+void affiche_menu();
+int Rechercher_animal();
+int addAnimal(Animal animaux[], int size);
+int Supprimer_animal(Animal animaux[], int nb_animaux);
+int Inventaire_par_espece(Animal animaux[], int nb_animaux);
+int Nettoyage_hebdomadaire(Animal animaux[], int nb_animaux);
+int Quitter_le_Menu();
 
 #endif //MAIN_H
