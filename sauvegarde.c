8f5e892575febf667356fdb96b74922732e8fcbe
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-11 01:05:52 +0200

    Update sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -53,7 +53,7 @@ int restauration(Animal *animals, int size) {
           return 1;
      }
 
-     FILE *fichier = fopen("animal.txt", "r");
+     FILE *fichier = fopen("animaux/animal.txt", "r");
      if (fichier == NULL) {
           printf("[ERROR] Impossible d'ouvrir le fichier animal.txt\n");
           return 1;
