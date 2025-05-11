a20a5d300b2641c9a756715f9a921cdc02c7d12d
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-11 17:44:42 +0200

    Update fonctionnalite.h

--- fonctionnalite.h
+++ fonctionnalite.h
@@ -14,19 +14,21 @@ typedef struct
  char nom[25];
  int espece;
  int b_year;
- int weight;
+ float weight;
  char phrase[255];
 } Animal;
 
 
 typedef enum
 {
-IGNORER = -1,
-INDEFINI = 0,
-HAMSTERE = 1,
-AUTRUCHE = 2,
-CHAT = 3,
-CHIEN = 4
+ CHIEN = 1,
+ CHAT = 2,
+ AUTRUCHE = 3,
+ HAMSTERE = 4
 } Espece;
+
+int* Rechercher_animal(Animal* tab, int nb_elements,char* nom, int espece, int type);
+/*int ajout_animal(Animal* tab, char* nom, int espece, int b_year, int weight, char* phrase);
+int delete_an_animal(Animal* tab, int id);*/
 
 #endif
