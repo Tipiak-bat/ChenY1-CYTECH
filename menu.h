8275cec75da3ab09c44b03135a5b46ac4e1e9b65
Tipiak-bat <176969935+Tipiak-bat@users.noreply.github.com>
2025-05-11 17:46:02 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 51
+#define TAILLE 51
 
 
 typedef struct Animal {
@@ -26,15 +26,18 @@ typedef enum
 } Espece;
 
 
-int Rechercher_animal();
-Animal ajouter_Animal();
-int Supprimer_animal(Animal * animaux, int nb_animaux);
+int Menu_Rechercher_animal(Animal * animaux, int taille);
+int Menu_Ajouter_Animal(Animal *animaux, int taille);
+int Menu_Supprimer_animal(Animal * animaux, int nb_animaux);
 int Inventaire_par_espece(Animal * animaux, int nb_animaux);
 int Nettoyage_hebdomadaire(Animal * animaux, int nb_animaux);
+int Imprimer_animaux(Animal *animaux, int taille);
 int Quitter_le_Menu(Animal * animaux, int nb_animaux);
 void affiche_Menu();
-int restauration(Animal *animals, int size);
-int sauvegarde(Animal *animals, int size);
+int restauration(Animal *animals, int taille);
+int sauvegarde(Animal *animals, int taille);
 int menu_sauv_rest(Animal *animaux, int nb_animaux);
+char* scan_nom();
+int* Rechercher_animal(Animal* tab, int nb_elements,char* nom, int espece, int type);
 
 #endif
